a285855eb83be7ecce9c4831e60e62de9d958c34
sangjinKIM93 <larger611@gmail.com>
2024-07-12 21:26:06 +0900

    Initial Commit

---
+++ DongSound/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DongSound
+//
+//  Created by 김상진 on 7/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DongSound/DongSoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  DongSoundApp.swift
+//  DongSound
+//
+//  Created by 김상진 on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DongSoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
